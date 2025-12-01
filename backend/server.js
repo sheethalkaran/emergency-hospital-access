bbaeabd52b95e22718a27d2466658b4f1f6f6623
sheethalkaran <sheethalkaran55@gmail.com>
2025-12-01 23:12:24 +0530

    Initial commit - Hospital Search App

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const { generateConfirmationPDF } = require('./utils/pdfGenerator');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
